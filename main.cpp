737bd33136217d8cb1632a1f6fc5ca8047a046d3
Erick-Lascano <119689561+Erick-Lascano@users.noreply.github.com>
2022-12-02 10:16:22 -0600

    Add a validation Loop

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ int main ()  {
  for (int c = 1; c < n; c++) { 
         cout << c << endl; 
     }   // add counter
+ 
+    do { 
+        cout << "How may numbers are you going to type "; 
+        cin>> n; 
+    } while (n < 1 || n > 10); 
   
     return 0; 
 } 
